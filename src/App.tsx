175dd6db329e96091770c508ce8d2db5f2765ff1
fevi_1spatial <federico.viscomi@1spatial.com>
2024-10-10 14:44:50 +0100

    8

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { MuiButton } from './components/MuiButton';
+import { MuiSelect } from './components/MuiSelect';
 import { MuiTextField } from './components/MuiTextField';
 import { MuiTypography } from './components/MuiTypography';
 
@@ -15,7 +16,9 @@ function App() {
       {/* <div>
         <MuiTextField></MuiTextField>
       </div>       */}
-      
+      <div>
+        <MuiSelect></MuiSelect>
+      </div>
     </div>
   );
 }
---
+++ src/components/MuiSelect.tsx
@@ -0,0 +1,29 @@
+import { Box, MenuItem, TextField } from "@mui/material";
+import { useState } from "react";
+
+export const MuiSelect = () => {
+  const [countries, setCountries] = useState<string[]>([]);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setCountries(typeof value === "string" ? value.split(",") : value);
+  };
+  return (
+    <Box width="250px">
+      <TextField
+        label="Select country"
+        select
+        value={countries}
+        onChange={handleChange}
+        fullWidth
+        slotProps={{ select: { multiple: true } }}
+        size="small"
+        color="success"
+        helperText="please select asdf"
+      >
+        <MenuItem value="IN">India</MenuItem>
+        <MenuItem value="US">USA</MenuItem>
+        <MenuItem value="AU">Australia</MenuItem>
+      </TextField>
+    </Box>
+  );
+};

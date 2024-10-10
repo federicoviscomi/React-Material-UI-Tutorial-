84612897dd81cf2a9b1f775e0cffff0f2f97a997
federicoviscomi <federicoviscomi@gmail.com>
2024-10-10 09:40:13 +0100

    4

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+export const MuiTypography = () => {
+    return (
+        <div>
+            <Typography variant='h1'>h1 Heading</Typography>
+            <Typography variant='h2'>h2 Heading</Typography>
+            <Typography variant='h3'>h3 Heading</Typography>
+            <Typography variant='h4'>h4 Heading</Typography>
+            <Typography variant='h5'>h5 Heading</Typography>
+            <Typography variant='h6'>h6 Heading</Typography>
+
+            <Typography variant='subtitle1'>Subtitle 1</Typography>
+            <Typography variant='subtitle2'>Subtitle 2</Typography>
+
+            <Typography variant='body1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe error laudantium non nemo suscipit, omnis mollitia? Et debitis quas dolorum obcaecati nisi nemo alias quaerat delectus numquam. Temporibus, repudiandae consequatur?</Typography>
+            <Typography variant='body2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe error laudantium non nemo suscipit, omnis mollitia? Et debitis quas dolorum obcaecati nisi nemo alias quaerat delectus numquam. Temporibus, repudiandae consequatur?</Typography>
+        </div>
+    );
+};
